cbb47db958896987b53a23b84b0c0531402aad76
Sephens <adesephens@gmail.com>
2023-03-16 22:26:13 +0300

    update

--- Firebase/Firebase.js
+++ Firebase/Firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: "AIzaSyAaBse7bI0yD_-usK9ed9tfvnG65ncRyH4",
-  authDomain: "code-crumbs.firebaseapp.com",
-  projectId: "code-crumbs",
-  storageBucket: "code-crumbs.appspot.com",
-  messagingSenderId: "202918254414",
-  appId: "1:202918254414:web:752dafd2ae86953a1ff01f",
-  measurementId: "G-J1PG6FYNS6"
+  apiKey: "AIzaSyDjHgQ6LFMH05IMgehSdsNg2nJVUJXnURE",
+  authDomain: "code-crumbs-8fe58.firebaseapp.com",
+  projectId: "code-crumbs-8fe58",
+  storageBucket: "code-crumbs-8fe58.appspot.com",
+  messagingSenderId: "120688422905",
+  appId: "1:120688422905:web:c60325ffa000d000dcd728",
+  measurementId: "G-PEPRTCWYY4"
 };
 
 // Initialize Firebase
